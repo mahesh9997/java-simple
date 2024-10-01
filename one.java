0531584a007101bbe5ea96a291e8cb9c93a39b65
mahesh9997 <maheshu4u@gmail.com>
2024-10-01 22:11:49 +0530

    java project

---
+++ one.java
@@ -0,0 +1,15 @@
+
+public class one {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Mahesh");
+		int i=50;
+		for(int k=0;k<=i;k++) {
+			System.out.println(k);
+		}
+		
+
+	}
+
+}
